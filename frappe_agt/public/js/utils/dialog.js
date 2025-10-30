a9f4b3ed6bafb16d6884ae0559310f29838470f1
Marco <gasparmarco92@gmail.com>
2025-10-30 20:51:21 +0000

    feat: Refactor text utility functions and add workflow pre-action and validation scripts
    - Removed source maps for text utility and table utility.
    - Updated text utility functions for better readability and consistency.
    - Introduced new workflow pre-action script to handle pre-action routines.
    - Added workflow validation script to ensure required fields are filled based on workflow state and action.

--- frappe_agt/public/js/utils/dialog.js
+++ frappe_agt/public/js/utils/dialog.js
@@ -1,66 +1,67 @@
-frappe.provide('agt.utils.dialog');
+// Copyright (c) 2025, AnyGridTech and contributors
+// For license information, please see license.txt
+frappe.provide("agt.utils.dialog");
 agt.utils.dialog.created = [];
-agt.utils.dialog.load = function (diagConfig) {
-    const dialog = new frappe.ui.Dialog(diagConfig);
-    agt.utils.dialog.created.push(dialog);
-    $(dialog['wrapper']).on('hidden.bs.modal', () => {
-        const index = agt.utils.dialog.created.indexOf(dialog);
-        if (index > -1) {
-            agt.utils.dialog.created.splice(index, 1);
-            agt.utils.dialog.refresh_dialog_stacking();
-        }
-    });
-    dialog.show();
-    setTimeout(() => agt.utils.dialog.refresh_dialog_stacking(), 50);
-    return dialog;
-};
-agt.utils.dialog.close_all = function () {
-    const dialogs = [...agt.utils.dialog.created];
-    dialogs.forEach((dialog) => {
-        if (dialog) {
-            dialog.hide();
-            const $backdrop = $(dialog['wrapper']).data("bs.modal")?.$backdrop;
-            $backdrop?.remove();
-        }
-    });
-    agt.utils.dialog.created = [];
-};
-agt.utils.dialog.close_by_title = function (title) {
-    const dialogIndex = agt.utils.dialog.created.findIndex((d) => d.title === title);
-    if (dialogIndex >= 0) {
-        const dialog = agt.utils.dialog.created[dialogIndex];
-        if (dialog) {
-            dialog.hide();
-            const $backdrop = $(dialog['wrapper']).data("bs.modal")?.$backdrop;
-            $backdrop?.remove();
-            agt.utils.dialog.created.splice(dialogIndex, 1);
-            agt.utils.dialog.refresh_dialog_stacking();
-        }
+agt.utils.dialog.load = function(diagConfig) {
+  const dialog = new frappe.ui.Dialog(diagConfig);
+  agt.utils.dialog.created.push(dialog);
+  $(dialog["wrapper"]).on("hidden.bs.modal", () => {
+    const index = agt.utils.dialog.created.indexOf(dialog);
+    if (index > -1) {
+      agt.utils.dialog.created.splice(index, 1);
+      agt.utils.dialog.refresh_dialog_stacking();
     }
+  });
+  dialog.show();
+  setTimeout(() => agt.utils.dialog.refresh_dialog_stacking(), 50);
+  return dialog;
 };
-agt.utils.dialog.show_debugger_alert = function (_frm, message, indicator, timeout = 10) {
-    if (frappe.user.has_role(['IT', 'Administrator', 'System Manager'])) {
-        frappe.show_alert({ message: __(message), indicator: indicator }, timeout);
+agt.utils.dialog.close_all = function() {
+  const dialogs = [...agt.utils.dialog.created];
+  dialogs.forEach((dialog) => {
+    if (dialog) {
+      dialog.hide();
+      const $backdrop = $(dialog["wrapper"]).data("bs.modal")?.$backdrop;
+      $backdrop?.remove();
     }
+  });
+  agt.utils.dialog.created = [];
 };
-agt.utils.dialog.refresh_dialog_stacking = function () {
-    if (typeof $ === 'undefined') {
-        console.error('jQuery is not loaded. Unable to refresh dialog stacking.');
-        return;
+agt.utils.dialog.close_by_title = function(title) {
+  const dialogIndex = agt.utils.dialog.created.findIndex((d) => d.title === title);
+  if (dialogIndex >= 0) {
+    const dialog = agt.utils.dialog.created[dialogIndex];
+    if (dialog) {
+      dialog.hide();
+      const $backdrop = $(dialog["wrapper"]).data("bs.modal")?.$backdrop;
+      $backdrop?.remove();
+      agt.utils.dialog.created.splice(dialogIndex, 1);
+      agt.utils.dialog.refresh_dialog_stacking();
     }
-    const baseModalIndex = 1051;
-    const baseBackdropIndex = 1050;
-    agt.utils.dialog.created.forEach((dialog, index) => {
-        if ($(dialog['wrapper']).is(':visible')) {
-            const zIndex = baseModalIndex + (index * 2);
-            $(dialog['wrapper']).css('z-index', zIndex);
-            const modalId = $(dialog['wrapper']).attr('id');
-            $(`.modal-backdrop[data-modal-id="${modalId}"]`).css('z-index', baseBackdropIndex + (index * 2));
-            const $backdrop = $(dialog['wrapper']).data("bs.modal")?.$backdrop;
-            if ($backdrop && !$backdrop.attr('data-modal-id')) {
-                $backdrop.attr('data-modal-id', modalId);
-            }
-        }
-    });
+  }
 };
-//# sourceMappingURL=dialog.js.map
+agt.utils.dialog.show_debugger_alert = function(_frm, message, indicator, timeout = 10) {
+  if (frappe.user.has_role(["IT", "Administrator", "System Manager"])) {
+    frappe.show_alert({ message: __(message), indicator }, timeout);
+  }
+};
+agt.utils.dialog.refresh_dialog_stacking = function() {
+  if (typeof $ === "undefined") {
+    console.error("jQuery is not loaded. Unable to refresh dialog stacking.");
+    return;
+  }
+  const baseModalIndex = 1051;
+  const baseBackdropIndex = 1050;
+  agt.utils.dialog.created.forEach((dialog, index) => {
+    if ($(dialog["wrapper"]).is(":visible")) {
+      const zIndex = baseModalIndex + index * 2;
+      $(dialog["wrapper"]).css("z-index", zIndex);
+      const modalId = $(dialog["wrapper"]).attr("id");
+      $(`.modal-backdrop[data-modal-id="${modalId}"]`).css("z-index", baseBackdropIndex + index * 2);
+      const $backdrop = $(dialog["wrapper"]).data("bs.modal")?.$backdrop;
+      if ($backdrop && !$backdrop.attr("data-modal-id")) {
+        $backdrop.attr("data-modal-id", modalId);
+      }
+    }
+  });
+};
--- frappe_agt/public/js/utils/index.js
+++ frappe_agt/public/js/utils/index.js
@@ -1,9 +1,10 @@
+// Copyright (c) 2025, AnyGridTech and contributors
+// For license information, please see license.txt
 import "./brazil";
+import "./text";
 import "./core";
 import "./db";
 import "./dialog";
 import "./doc";
 import "./form";
 import "./table";
-import "./text";
-//# sourceMappingURL=index.js.map
