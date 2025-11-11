0366b520c62111fec6a5e1e4ad7ec3dc6fb9d8eb
Luigi Azor <146014608+luigiazoreng@users.noreply.github.com>
2025-11-11 16:22:39 +0000

    Remove deprecated utility scripts and consolidate workflow validation logic into TypeScript bundle
    - Deleted utility scripts: db.js, dialog.js, doc.js, form.js, index.js, table.js, text.js, pre_action.js, validate.js
    - Consolidated workflow validation and pre-action logic into a new TypeScript bundle (frappe_agt.bundle.ts)
    - Improved code organization and maintainability by transitioning to TypeScript for better type safety and structure

--- frappe_agt/public/js/utils/brazil/index.js
+++
@@ -1,7 +0,0 @@
-// Copyright (c) 2025, AnyGridTech and contributors
-// For license information, please see license.txt
-import "./cnpj";
-import "./cpf";
-import "./cpf_cnpj";
-import "./cep";
-import "./phone";
