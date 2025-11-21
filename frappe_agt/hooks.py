d8521733f956e3ad7bea1f59a97344e5a03eed68
Marco <gasparmarco92@gmail.com>
2025-11-21 19:42:02 +0000

    Additional handling of 404 errors with a custom page and removal of obsolete code.

--- frappe_agt/hooks.py
+++ frappe_agt/hooks.py
@@ -242,9 +242,10 @@ app_include_js = "assets/frappe_agt/js/app.bundle.js"
 # 	"Logging DocType Name": 30  # days to retain logs
 # }
 
-# força qualquer 404 (site ou desk) a usar sua página personalizada
-website_catch_all = "my_app.www.404"
-
+# Handle 404 errors with a custom page
 error_handlers = {
-    "404": "my_app.utils.error404"
+    "404": "frappe_agt.www.not_found"
 }
+
+# Catch all route
+website_catch_all = "not_found"
--- frappe_agt/www/not_found.py
+++ frappe_agt/www/not_found.py
@@ -1,4 +1,4 @@
 def get_context(context):
     context.no_cache = 1
     context.title = "Página não encontrada"
-    context.message = "O recurso solicitado não existe."
+    return context
